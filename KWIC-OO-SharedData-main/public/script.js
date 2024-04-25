e18fc74959fda2ede63ab2e9408a2f83161fc9a0
Samuel Le <tuongvy3252@gmail.com>
2024-04-24 23:14:59 -0500

    second

--- KWIC-OO-SharedData-main/public/script.js
+++
@@ -1,4 +0,0 @@
-import { onClickClearButton, onSubmitForm } from "./controller/home_controller.js";
-
-document.querySelector('#form-input').onsubmit = onSubmitForm;
-document.getElementById('clearButton').onclick = onClickClearButton;
